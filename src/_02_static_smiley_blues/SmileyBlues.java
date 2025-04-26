d600ffa9dda55efc020dd09eca2d8d8d0490d343
opie10 <augie@keatinge.com>
2025-04-26 16:29:33 -0700

    I finished two lessons in two classes

--- src/_02_static_smiley_blues/SmileyBlues.java
+++ src/_02_static_smiley_blues/SmileyBlues.java
@@ -11,6 +11,8 @@ public class SmileyBlues {
 		JPanel panel;
 		// 4. Add your panel to your frame
 		frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new JPanel();
 		frame.add(panel);
 		// 8. DO THIS LAST: 
@@ -19,7 +21,15 @@ public class SmileyBlues {
 		// Run it again; are your Smileys all yellow now? Also try changing them to red or green!
 
 		// 5. Make three Smiley objects and add them to your panel
-Smiley smile1= new Smiley();
+		
+		Smiley smile1= new Smiley() ;
+		Smiley smile2=new Smiley();
+		Smiley smile3= new Smiley();
+
+		panel.add(smile1);
+		panel.add(smile2);
+		panel.add(smile3);
+		frame.pack();
 		// 6. Pack your frame
 
 		// 7. RUN THE PROGRAM and make sure you see three blue Smileys!
---
+++ src/_04_netflix/MovieRunner.java
@@ -0,0 +1,9 @@
+package _04_netflix;
+
+public class MovieRunner {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ß
+	}
+
+}
